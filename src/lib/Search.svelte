59422b5b1ea1d268e46f31418de5fea47c4a5671
Joseph Brooksbank <joseph.brooksbank@navexglobal.com>
2022-12-08 17:38:21 -0800

    add search bar stub

---
+++ src/lib/Search.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    let value: string;
+
+    var delayTimer: NodeJS.Timeout;
+
+    const handleSearch = (searchTerm: any) => {
+            clearTimeout(delayTimer);
+            delayTimer = setTimeout(() => {
+                alert('searched');
+            }, 1000);
+    }
+</script>
+
+<input bind:value on:input={handleSearch}>
+
+<style>
+    input {
+        width: 100%;
+        border-radius: 10px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
 	import Card from '$lib/Card.svelte';
 	import CardPicker from '$lib/CardPicker.svelte';
+	import Search from '$lib/Search.svelte';
 </script>
 
-<h1>Movie Picker!</h1>
-<p>Vote on which movie we should watch next week :)</p>
-
 <div>
-	<CardPicker>
-	</CardPicker>
+	<h1>Movie Picker!</h1>
+	<Search />
+	<p>Vote on which movie we should watch next week :)</p>
+	<CardPicker />
 </div>
 
 <style>
-    :root {
-        --bg-color: #282c34;
-        background-color: var(--bg-color);
-    }
-    div {
-        align-items: center;
-        display: flex;
-        justify-content: center;
-        height: 100%;
-        margin: 0px;
-        overflow: visible ;
-        padding: 0px;
-    }
+	:root {
+		--bg-color: #282c34;
+		background-color: var(--bg-color);
+	}
+	div {
+		gap: 10px;
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		height: 100%;
+		margin: 0px;
+		overflow: visible;
+		padding: 0px;
+	}
 	h1 {
 		text-align: center;
 	}
-    p {
-        text-align: center;
-    }
+	p {
+		text-align: center;
+	}
 	:global(body) {
-        height: 100%;
+		height: 100%;
 		color: azure;
 		font-family: Arial, Helvetica, sans-serif;
 	}
