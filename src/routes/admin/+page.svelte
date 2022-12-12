97c22e455e771bc2d91c58a1443f8b872ad08e23
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2022-12-11 23:26:36 -0600

    Begin admin page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,16 @@
+<h1>Admin Settings</h1>
+
+
+<style>
+	:root {
+		--bg-color: #282c34;
+		background-color: var(--bg-color);
+        color: azure;
+        font-family: Arial, Helvetica, sans-serif;
+	}
+
+    h1 {
+        text-align: center;
+    }
+
+</style>
