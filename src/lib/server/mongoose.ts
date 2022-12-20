e2a4e569f206f23413b6c1eb1b3ffd7dcf64f261
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2022-12-19 22:18:27 -0600

    Fix update; await promises

--- src/lib/server/mongoose.ts
+++ src/lib/server/mongoose.ts
@@ -1,34 +1,43 @@
-import { Movie, type IMovie } from "$lib/schema/movie.schema";
+import { Movie, type IMovie } from '$lib/schema/movie.schema';
 import type mongoose from 'mongoose';
-import type { TMDBMovie } from "src/types/TMDB";
+import type { TMDBMovie } from 'src/types/TMDB';
 
-
-export const createMovie = async(document: TMDBMovie) => {
-    // Adding movie if it doesn't exist
-    const query: mongoose.FilterQuery<IMovie> = {id: document.id};
-    // starting with 0 votes and 2 priority if not specified
-    const update: mongoose.UpdateQuery<IMovie> = {$setOnInsert: {votes: 0, priority: 2, ...document }};
-    const options: mongoose.QueryOptions = {upsert: true, new: true, setDefaultsOnInsert: true};
-    const response = await Movie.findOneAndUpdate(query, update, options);
-    console.log(JSON.stringify(response));
-    return response;
-}
+export const createMovie = async (document: TMDBMovie) => {
+	// Adding movie if it doesn't exist
+	const query: mongoose.FilterQuery<IMovie> = { id: document.id };
+	// starting with 0 votes and 2 priority if not specified
+	const update: mongoose.UpdateQuery<IMovie> = {
+		$setOnInsert: { votes: 0, priority: 2, ...document }
+	};
+	const options: mongoose.QueryOptions = { upsert: true, new: true, setDefaultsOnInsert: true };
+	const response = await Movie.findOneAndUpdate(query, update, options);
+	console.log(JSON.stringify(response));
+	return response;
+};
 
 export const getMovies: (count?: number) => Promise<IMovie[]> = async (count?: number) => {
-    let query = Movie.find().sort({priority: 1, dateAdded: 1}).lean()
-    if (count) {
-        query = query.limit(count);
-    } 
-    return JSON.parse(JSON.stringify(await query.exec()));
-}
+	let query = Movie.find().sort({ priority: 1, dateAdded: 1 }).lean();
+	if (count) {
+		query = query.limit(count);
+	}
+	return JSON.parse(JSON.stringify(await query.exec()));
+};
 
 export const updateMovies = async (documents: IMovie[]) => {
-    for (const doc of documents) {
-        Movie.updateOne({id: doc.id}, {$set: {votes: doc.votes, priority: doc.priority}})
-    }
-    return await getMovies();
-}
+
+    const promises = [];
+	for (const doc of documents) {
+		promises.push(Movie.updateOne(
+			{ id: doc.id },
+			{ $set: { votes: doc.votes, priority: doc.priority } }
+		));
+	}
+
+    await Promise.all(promises);
+
+	return await getMovies();
+};
 
 export const insertVote = async (document: IMovie) => {
-    return await Movie.updateOne({id: document.id}, {$inc: {votes: 1}});
-}
+	return await Movie.updateOne({ id: document.id }, { $inc: { votes: 1 } });
+};
