54d303a9885bbd76fb74836187fb7b1b783826e9
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2023-02-14 22:51:28 -0600

    Add hint text for info toggle

--- src/lib/InfoToggle/InfoToggle.svelte
+++ src/lib/InfoToggle/InfoToggle.svelte
@@ -9,7 +9,7 @@
 	};
 </script>
 
-<i class="fa-solid fa-circle-info fa-lg" on:click={handleClick} />
+<i class="fa-solid fa-circle-info fa-lg" on:click={handleClick} /><p>👈🏻 click this for movie info!</p>
 
 <style>
 	i:hover {
@@ -18,5 +18,8 @@
 	i {
 		cursor: pointer;
 		margin: 0;
+		margin-left: 10px;
+		margin-right: -10px;
 	}
+
 </style>
